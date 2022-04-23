749b8dc7027eaf33e52496b6badeb99593323f64
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-04-22 21:03:05 -0700

    Added Pokedex components

--- react_pokedex/src/Pokecard.js
+++ react_pokedex/src/Pokecard.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const pokeAPI =
-    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon";
-
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
 const Pokecard = (props) => {
-       let img = `${pokeAPI} ${props.id}.png`;
-    return (
+  let imgSrc = `${pokeAPI} ${props.id}.png`;
+  return (
     <div className="Pokecard">
-<div className='Pokecard-name'>{props.name}</div>
-<div className='Pokecard-img'>{props.img}</div>
-<div className='Pokecard-type'>{props.type}</div>
+      <div className="Pokecard-name"> {props.name} </div>
+      <div className="Pokecard-img"> {props.imgSrc} </div>
+      <div className="Pokecard-type"> {props.type} </div>
+      <div className="Pokecard-exp">{props.exp} </div>
     </div>
-    );
-}
+  );
+};
 
 export default Pokecard;
